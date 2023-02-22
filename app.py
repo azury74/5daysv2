58a30ce4268d9f3af060dc2085c91967e9e6e1ba
Louis Laurent <101343530+azury74@users.noreply.github.com>
2023-02-22 13:25:58 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,128 @@
+from flask import Flask,render_template,request, url_for, redirect, render_template,current_app, g,session,flash, send_from_directory,jsonify
+import sqlite3
+import click
+import os 
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd 
+import difflib
+from logging.handlers import RotatingFileHandler
+import logging
+import sqlalchemy as sa
+from click import echo
+from flask import Flask
+from flask.logging import default_handler
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+
+app = Flask(__name__)
+
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# create the extension
+app.secret_key = "az900"
+
+
+
+
+# Determine the folder of the top-level directory of this project
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    FLASK_ENV = 'development'
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.getenv('SECRET_KEY', default='BAD_SECRET_KEY')
+    # Since SQLAlchemy 1.4.x has removed support for the 'postgres://' URI scheme,
+    # update the URI to the postgres database to use the supported 'postgresql://' scheme
+    if os.getenv('DATABASE_URL'):
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    else:
+        SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, 'instance', 'app.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Logging
+    LOG_WITH_GUNICORN = os.getenv('LOG_WITH_GUNICORN', default=False)
+
+
+class ProductionConfig(Config):
+    FLASK_ENV = 'production'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI',
+                                        default=f"sqlite:///{os.path.join(BASEDIR, 'instance', 'test.db')}")
+    WTF_CSRF_ENABLED = False
+
+def configure_logging(app):
+    # Logging Configuration
+    if app.config['LOG_WITH_GUNICORN']:
+        gunicorn_error_logger = logging.getLogger('gunicorn.error')
+        app.logger.handlers.extend(gunicorn_error_logger.handlers)
+        app.logger.setLevel(logging.DEBUG)
+    else:
+        file_handler = RotatingFileHandler('instance/flask-user-management.log',
+                                           maxBytes=16384,
+                                           backupCount=20)
+        file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(threadName)s-%(thread)d: %(message)s [in %(filename)s:%(lineno)d]')
+        file_handler.setFormatter(file_formatter)
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+    # Remove the default logger configured by Flask
+    app.logger.removeHandler(default_handler)
+
+    app.logger.info('Starting the Flask User Management App...')
+
+
+
+    
+#---------------------------------------------------------------------------------------
+#Page de l'interface : 
+
+@app.route('/')
+def accueil():
+    return render_template('index.html')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+        
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            return redirect(url_for('logo', info="Pas de fichier détecté!"))
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('logo', info="None"))
+        
+        else :
+            return redirect(url_for('logo', info="Pas la bonne extension !"))
+   
+
+@app.route('/logo')
+def logo():
+    info=request.args.get('info')
+    return render_template('logo.html',info=info)
+
+if __name__ == '__main__':
+    app.run()
+
+    
